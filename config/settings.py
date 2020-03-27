885e0bc6502e257925a5265feafce71b7e344744
hminn <hmkim0008@gmail.com>
2020-03-27 22:51:18 +0900

    Update rooms application  && Create core application

--- config/settings.py
+++ config/settings.py
@@ -39,9 +39,13 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = ["django_countries"]
 
-PROJECT_APPS = ["users.apps.UsersConfig", "rooms.apps.RoomsConfig"]
+PROJECT_APPS = [
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+    "core.apps.CoreConfig",
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
