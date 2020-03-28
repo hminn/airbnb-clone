1b2d3ebfb3ef194b0426fe365fc3b71870446745
hminn <hmkim0008@gmail.com>
2020-03-28 18:43:29 +0900

    First finished Models

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,10 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "core.apps.CoreConfig",
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
