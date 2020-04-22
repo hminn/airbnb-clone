59f67b06943cb6e1ec8492bfda494ea6d11b0cdf
hminn <hmkim0008@gmail.com>
2020-04-23 01:02:15 +0900

    Design Headerbar Using Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	theme: {
+		extend: {
+			spacing: {
+				"25vh": "25vh",
+				"50vh": "50vh",
+				"75vh": "75vh",
+			},
+			borderRadius: {
+				xl: "1.5rem",
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
